2719b050389b0ac8ebba50963ab706e70342a1ba
enrn3199 <nramiren@everis.com>
2021-03-04 11:45:48 -0300

    first commit

---
+++ app/src/main/java/com/example/tmdbmadproject/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.tmdbmadproject.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.tmdbmadproject.R
+import com.example.tmdbmadproject.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment : Fragment() {
+    private var _binding: FragmentNotificationsBinding? = null
+    private val binding get() = _binding!!
+
+    private val notificationsViewModel by viewModels<NotificationsViewModel>()
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+
+        notificationsViewModel.text.observe(viewLifecycleOwner, { binding.textNotifications.text = it })
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
