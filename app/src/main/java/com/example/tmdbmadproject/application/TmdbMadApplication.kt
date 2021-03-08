fdbfb39c41fd36ff7ee84118b9f4dfd46086cbbc
enrn3199 <nramiren@everis.com>
2021-03-08 12:24:53 -0300

    agregado Hilt como libreria para Dependency Injection

---
+++ app/src/main/java/com/example/tmdbmadproject/application/TmdbMadApplication.kt
@@ -0,0 +1,8 @@
+package com.example.tmdbmadproject.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TmdbMadApplication: Application() {
+}
--- app/src/main/java/com/example/tmdbmadproject/di/NetworkModule.kt
+++ app/src/main/java/com/example/tmdbmadproject/di/NetworkModule.kt
@@ -1,18 +1,28 @@
 package com.example.tmdbmadproject.di
 
 import com.example.tmdbmadproject.data.api.TmdbApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
-
+@Module
+@InstallIn(SingletonComponent::class)
 class NetworkModule {
 
+    @Provides
+    @Singleton
     fun providesTmdbApi(retrofit: Retrofit): TmdbApi {
         return retrofit.create(TmdbApi::class.java)
     }
 
+    @Provides
+    @Singleton
     fun providesRetrofit(client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
@@ -20,12 +30,14 @@ class NetworkModule {
             .build()
     }
 
+    @Provides
     fun providesOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .build()
     }
 
+    @Provides
     fun providesLoggingInterceptor(): HttpLoggingInterceptor {
         return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
     }
