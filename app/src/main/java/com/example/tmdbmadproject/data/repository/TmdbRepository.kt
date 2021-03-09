97b6823c41e1a4554b7b978311db826fed4d2a11
enrn3199 <nramiren@everis.com>
2021-03-09 16:23:38 -0300

    concatenando llamadas de Flow para una vez obtenida la lista de peliculas, obtener entonces la configuracion de Tmdb y asignar la url del poster a cada pelicula de la lista

--- app/src/main/java/com/example/tmdbmadproject/data/repository/TmdbRepository.kt
+++ app/src/main/java/com/example/tmdbmadproject/data/repository/TmdbRepository.kt
@@ -2,10 +2,17 @@ package com.example.tmdbmadproject.data.repository
 
 import com.example.tmdbmadproject.data.datasource.TmdbDataSource
 import com.example.tmdbmadproject.data.model.MovieResume
+import com.example.tmdbmadproject.data.model.generatePosterUrl
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.flow.zip
 import javax.inject.Inject
 
-class TmdbRepository @Inject constructor(private val popularMoviesDataSource: TmdbDataSource) {
+class TmdbRepository @Inject constructor(private val dataSource: TmdbDataSource) {
 
-    fun getPopularMovies(): Flow<List<MovieResume>> = popularMoviesDataSource.getPopularMovies()
+    fun getPopularMovies(): Flow<List<MovieResume>> = dataSource.getPopularMovies()
+        .zip(dataSource.getTmdbConfiguration()) { movies, configuration ->
+            movies.onEach { movieResume -> movieResume.posterUrl = movieResume.generatePosterUrl(configuration) }
+        }
 }
