4a80afd93c3d6f981a6f35a17d1c7373ab5694b9
enrn3199 <nramiren@everis.com>
2021-03-09 16:21:00 -0300

    creado metodo getTmdbConfiguration en el data source para obtener datos de configuracion de las imagenes como los poster de las peliculas

--- app/src/main/java/com/example/tmdbmadproject/data/datasource/RemoteTmdbDataSource.kt
+++ app/src/main/java/com/example/tmdbmadproject/data/datasource/RemoteTmdbDataSource.kt
@@ -1,17 +1,31 @@
 package com.example.tmdbmadproject.data.datasource
 
 import com.example.tmdbmadproject.data.Constants
+import com.example.tmdbmadproject.data.InMemoryCache
 import com.example.tmdbmadproject.data.api.TmdbApi
 import com.example.tmdbmadproject.data.model.MovieResume
+import com.example.tmdbmadproject.data.model.TmdbConfiguration
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class RemoteTmdbDataSource @Inject constructor(private val api: TmdbApi) : TmdbDataSource {
+class RemoteTmdbDataSource @Inject constructor(private val api: TmdbApi, private val inMemoryCache: InMemoryCache) : TmdbDataSource {
     override fun getPopularMovies(): Flow<List<MovieResume>> = flow {
         val response = api.getMovies(sortBy = Constants.MOST_POPULAR_MOVIES, Constants.API_KEY)
         val movies = response.results ?: emptyList()
 
         emit(movies)
     }
+
+    override fun getTmdbConfiguration(): Flow<TmdbConfiguration> = flow {
+        if (inMemoryCache.getTmdbConfiguration() == null) {
+            val configuration = api.getConfiguration(Constants.API_KEY)
+            inMemoryCache.setTmdbConfiguration(configuration)
+        }
+
+        val tmdbConfiguration = inMemoryCache.getTmdbConfiguration() ?: TmdbConfiguration()
+
+        emit(tmdbConfiguration)
+    }
 }
+
--- app/src/main/java/com/example/tmdbmadproject/data/datasource/TmdbDataSource.kt
+++ app/src/main/java/com/example/tmdbmadproject/data/datasource/TmdbDataSource.kt
@@ -1,9 +1,12 @@
 package com.example.tmdbmadproject.data.datasource
 
 import com.example.tmdbmadproject.data.model.MovieResume
+import com.example.tmdbmadproject.data.model.TmdbConfiguration
 import kotlinx.coroutines.flow.Flow
 
 
 interface TmdbDataSource {
     fun getPopularMovies(): Flow<List<MovieResume>>
+
+    fun getTmdbConfiguration(): Flow<TmdbConfiguration>
 }
