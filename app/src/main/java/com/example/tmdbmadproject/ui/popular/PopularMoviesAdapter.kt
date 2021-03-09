5771b0ca0e913965edfedef2a37b3ef19dd7f565
enrn3199 <nramiren@everis.com>
2021-03-09 13:15:43 -0300

    creado adapter para lista de peliculas

---
+++ app/src/main/java/com/example/tmdbmadproject/ui/popular/PopularMoviesAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.tmdbmadproject.ui.popular
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tmdbmadproject.data.model.MovieResume
+import com.example.tmdbmadproject.databinding.ListItemMovieBinding
+
+class PopularMoviesAdapter(private val listener: (movie: MovieResume) -> Unit) : RecyclerView.Adapter<PopularMoviesViewHolder>() {
+    private val dataSet: MutableList<MovieResume> = mutableListOf()
+
+    fun loadMovies(movies: List<MovieResume>) {
+        dataSet.clear()
+        dataSet.addAll(movies)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularMoviesViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemMovieBinding.inflate(inflater, parent, false)
+
+        return PopularMoviesViewHolder(binding, listener)
+    }
+
+    override fun onBindViewHolder(holder: PopularMoviesViewHolder, position: Int) {
+        holder.bind(movie = dataSet[position])
+    }
+
+    override fun getItemCount(): Int {
+        return if (dataSet.isEmpty()) 0 else dataSet.size
+    }
+}
+
+class PopularMoviesViewHolder(
+    private val binding: ListItemMovieBinding,
+    private val listener: (movie: MovieResume) -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(movie: MovieResume) {
+        binding.movieTitle.text = movie.title
+        binding.movieDescription.text = movie.overview
+        itemView.setOnClickListener { listener.invoke(movie) }
+    }
+}
