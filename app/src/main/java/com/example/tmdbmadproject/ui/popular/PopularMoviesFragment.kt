38f3ca32ca7398b454c22b86112820e916ae36a2
enrn3199 <nramiren@everis.com>
2021-03-09 13:17:44 -0300

    primera iteracion de MVI

--- app/src/main/java/com/example/tmdbmadproject/ui/popular/PopularMoviesFragment.kt
+++ app/src/main/java/com/example/tmdbmadproject/ui/popular/PopularMoviesFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tmdbmadproject.R
+import com.example.tmdbmadproject.data.model.MovieResume
 import com.example.tmdbmadproject.databinding.FragmentPopularMoviesBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,24 +18,50 @@ class PopularMoviesFragment : Fragment() {
     private var _binding: FragmentPopularMoviesBinding? = null
     private val binding get() = _binding!!
 
-    private val homeViewModel by viewModels<PopularMoviesViewModel>()
+    private val viewModel by viewModels<PopularMoviesViewModel>()
+
+    private val moviesAdapter = PopularMoviesAdapter(this::onMovieSelected)
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentPopularMoviesBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         setupViews()
         setupLiveDataObservers()
 
-        return binding.root
+        viewModel.sendIntention(ViewIntention.LoadPopularMovies)
     }
 
-    private fun setupViews() {
-        binding.detailsButton.setOnClickListener { findNavController().navigate(R.id.nav_from_home_to_detail) }
+
+    private fun setupViews() = with(binding) {
+        popularMoviesRetryButton.setOnClickListener { viewModel.sendIntention(ViewIntention.LoadPopularMovies) }
+        popularMoviesRecyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            adapter = moviesAdapter
+        }
     }
 
     private fun setupLiveDataObservers() {
-        homeViewModel.text.observe(viewLifecycleOwner) { binding.textHome.text = it }
+        viewModel.viewState.observe(viewLifecycleOwner) { handleViewState(it) }
+    }
+
+    private fun handleViewState(viewState: ViewState) = with(binding) {
+        moviesAdapter.loadMovies(viewState.movies)
+
+        popularMoviesProgressBar.apply { if (viewState.isLoading) show() else hide() }
+
+        popularMoviesErrorTextView.visibility = if (viewState.isError) View.VISIBLE else View.GONE
+        popularMoviesRetryButton.visibility = if (viewState.isError) View.VISIBLE else View.GONE
+    }
+
+    private fun onMovieSelected(movie: MovieResume) {
+        findNavController().navigate(R.id.nav_from_home_to_detail)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/tmdbmadproject/ui/popular/PopularMoviesViewModel.kt
+++ app/src/main/java/com/example/tmdbmadproject/ui/popular/PopularMoviesViewModel.kt
@@ -3,11 +3,74 @@ package com.example.tmdbmadproject.ui.popular
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tmdbmadproject.data.model.MovieResume
+import com.example.tmdbmadproject.data.repository.TmdbRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.properties.Delegates
 
 @HiltViewModel
-class PopularMoviesViewModel @Inject constructor() : ViewModel() {
-    private val _text = MutableLiveData<String>().apply { value = "This is popular movies Fragment" }
-    val text: LiveData<String> = _text
+class PopularMoviesViewModel @Inject constructor(private val repository: TmdbRepository) : ViewModel() {
+    private val _viewState = MutableLiveData<ViewState>()
+    val viewState: LiveData<ViewState> = _viewState
+
+    private var state: ViewState by Delegates.observable(initialValue = ViewState()) { _, _, new -> _viewState.value = new }
+
+    fun sendIntention(intention: ViewIntention) {
+        when (intention) {
+            ViewIntention.LoadPopularMovies -> {
+                if (state.movies.isEmpty()) {
+                    searchPopularMovies()
+                }
+            }
+        }
+    }
+
+    private fun sendAction(action: ViewAction) {
+        state = onReduceState(action)
+    }
+
+    private fun onReduceState(action: ViewAction): ViewState {
+        return when (action) {
+            is ViewAction.LoadingPopularMovies -> state.copy(isLoading = true, isError = false, movies = emptyList())
+            is ViewAction.ErrorLoadingPopularMovies -> state.copy(isLoading = false, isError = true, movies = emptyList())
+            is ViewAction.SuccessLoadingPopularMovies -> state.copy(isLoading = false, isError = false, movies = action.movies)
+        }
+    }
+
+    private fun searchPopularMovies() {
+        sendAction(ViewAction.LoadingPopularMovies)
+
+        viewModelScope.launch {
+            repository.getPopularMovies()
+                .catch { sendAction(ViewAction.ErrorLoadingPopularMovies) }
+                .collect { popularMovies -> sendAction(ViewAction.SuccessLoadingPopularMovies(popularMovies)) }
+        }
+    }
 }
+
+data class ViewState(
+    val isLoading: Boolean = true,
+    val isError: Boolean = false,
+    val movies: List<MovieResume> = emptyList()
+) : BaseViewState
+
+sealed class ViewAction : BaseViewAction {
+    object ErrorLoadingPopularMovies : ViewAction()
+    object LoadingPopularMovies : ViewAction()
+    data class SuccessLoadingPopularMovies(val movies: List<MovieResume>) : ViewAction()
+}
+
+sealed class ViewIntention : BaseViewIntention {
+    object LoadPopularMovies : ViewIntention()
+}
+
+interface BaseViewState
+
+interface BaseViewAction
+
+interface BaseViewIntention
