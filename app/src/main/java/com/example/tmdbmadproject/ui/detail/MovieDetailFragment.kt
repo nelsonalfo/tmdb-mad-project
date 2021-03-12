3e3be1d8abf56b836b84bec2536aa869bb9fc59e
enrn3199 <nramiren@everis.com>
2021-03-12 09:22:19 -0300

    optimizando imports

--- app/src/main/java/com/example/tmdbmadproject/ui/detail/MovieDetailFragment.kt
+++ app/src/main/java/com/example/tmdbmadproject/ui/detail/MovieDetailFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
-import com.bumptech.glide.Glide
 import com.example.tmdbmadproject.R
 import com.example.tmdbmadproject.base.BaseFragment
 import com.example.tmdbmadproject.base.loadFromUrl
